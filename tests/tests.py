6761315fb9883f37e8eeb7c673543bb727a725e3
davidkaggulire <dkaggulire@gmail.com>
2021-11-11 00:28:33 +0300

    run flake8

--- tests/tests.py
+++ tests/tests.py
@@ -5,11 +5,13 @@ import sys
 import unittest
 import json
 
+
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 from unittest.mock import patch, mock_open
 from patterns import DatabaseProvider
+
 
 class TestFileProvider(unittest.TestCase):
 
@@ -28,43 +30,43 @@ class TestFileProvider(unittest.TestCase):
         self.assertEqual(p1, p2)
         self.assertIsNotNone(p1)
 
-    @patch("builtins.open", new_callable=mock_open,
-       read_data=json.dumps({'district' : 'Kampala','weather' : "sunny"}))
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data=json.dumps({'district': 'Kampala', 'weather': "sunny"}))
     def test_create_success(self, mock_file):
         """success test for create operation"""
         file_data = {
-                    'district' : 'Kampala',
-                    'weather' : "sunny",
+                    'district': 'Kampala',
+                    'weather': "sunny",
                     }
         expected_output = (True, "Create operation successful")
         filename = 'example.json'
         actual_output = self.db.create(filename, file_data)
         mock_file.assert_called_with(filename, 'w')
-        
+
         self.assertEqual(expected_output, actual_output)
 
-    @patch("builtins.open", new_callable=mock_open,
-       read_data=json.dumps({'district' : 'Kampala','weather' : "sunny"}))
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data=json.dumps({'district': 'Kampala', 'weather': "sunny"}))
     def test_create_fail(self, mock_file):
         """success test for create operation"""
-        file_data = {'district' : 'Kampala','weather' : "sunny"}
-        expected_output = (False, "Create operation failed")
-        filename = '/etc/example.json'
-        # actual_output = self.db.create(filename, file_data)
-        # self.assertEqual(expected_output, actual_output)
+        pass
 
-    @patch("builtins.open", new_callable=mock_open,
-       read_data=json.dumps({'district' : 'Kampala','weather' : "sunny"}))
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data=json.dumps({'district': 'Kampala', 'weather': "sunny"}))
     def test_read_data_success(self, mock_file):
         """success test for read operation"""
-        file_data = {'district' : 'Kampala','weather' : "sunny"}
+        file_data = {'district': 'Kampala', 'weather': "sunny"}
         expected_output = (True, "Successfully read", {"output": file_data})
         filename = 'example.json'
         actual_output = self.db.read(filename)
 
-        # Assert filename is file that is opened
         mock_file.assert_called_with(filename)
-
         self.assertEqual(expected_output, actual_output)
 
     def test_read_data_fail(self):
@@ -75,18 +77,18 @@ class TestFileProvider(unittest.TestCase):
 
         self.assertEqual(expected_output, actual_output)
 
-    @patch("builtins.open", new_callable=mock_open,
-       read_data=json.dumps({'district' : 'Kampala','weather' : "sunny"}))
+    @patch(
+        "builtins.open",
+        new_callable=mock_open,
+        read_data=json.dumps({'district': 'Kampala', 'weather': "sunny"}))
     def test_update_success(self, mock_file):
         """test successful update"""
-        file_data = {'district' : 'Kampala','weather' : "sunny"}
+        file_data = {'district': 'Kampala', 'weather': "sunny"}
         expected_output = (True, "Successfully updated")
         filename = 'example.json'
         actual_output = self.db.update(filename, file_data)
 
-        # Assert filename is file that is opened
         mock_file.assert_called_with(filename, 'w')
-        
         self.assertEqual(expected_output, actual_output)
 
     def test_delete_nonexistent(self):
@@ -101,7 +103,7 @@ class TestFileProvider(unittest.TestCase):
     def test_remove_file(self):
         """test to delete file if it exists"""
         filename = 'example3.json'
-        file_data = {'district' : 'Kampala','weather' : "sunny"}
+        file_data = {'district': 'Kampala', 'weather': "sunny"}
         expected_output = (True, "Successfully deleted")
 
         self.db.create(filename, file_data)
