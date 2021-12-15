57268fbec3d5884750686d809c869d0ff201d245
davidkaggulire <dkaggulire@gmail.com>
2021-12-15 13:07:16 +0300

    refactor code to use firebase_admin sdk

--- storage/firebase_cloud_storage.py
+++ storage/firebase_cloud_storage.py
@@ -1,50 +1,56 @@
 # google_cloud_storage /firebase cloud storage
 
+from firebase_admin import credentials, initialize_app, storage
+from firebase_admin import auth
 import os
-import pyrebase
 from datetime import datetime, timedelta
 from typing import Tuple, List
+import requests
+import json
+import firebase_admin
 from .storage_interface import IStorage
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
 
 
 load_dotenv()
 
-config = {
-    "apiKey": os.environ.get('apiKey'),
-    "authDomain": os.environ.get('authDomain'),
-    "projectId": os.environ.get('projectId'),
-    "storageBucket": os.environ.get('storageBucket'),
-    "messagingSenderId": os.environ.get('messagingSenderId'),
-    "appId": os.environ.get('appId'),
-    "measurementId": os.environ.get('measurementId'),
-    "serviceAccount": os.environ.get('serviceAccount'),
-    "databaseURL": os.environ.get('databaseURL'),
-}
+config = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 
 
 class FirebaseStorage(IStorage):
 
     def __init__(self):
-        firebase = pyrebase.initialize_app(config)
-        self.storage = firebase.storage()
-        # create authentication
-        self.auth = firebase.auth()
+        cred = credentials.Certificate(config)
+
+        bucket_name = {
+            'storageBucket': os.environ.get('storageBucket')
+        }
+        try:
+            firebase_admin.get_app()
+        except ValueError:
+            initialize_app(cred, bucket_name)
+
+        self.api_key = os.environ.get('apiKey')
+
         # setting base folder
         self.base = "repo"
 
     def uploadFile(self, source: str, dest: str) -> Tuple[bool, str]:
         print("Uploading file....")
-        if self.auth.current_user is None:
+        if self.current_user is None:
             reason = "User should login first"
             print(reason)
             return False, reason
         else:
-            user_id = self.auth.current_user["localId"]
+            user_id = self.current_user["localId"]
             print(f"user_id is {user_id}")
 
             try:
-                self.storage.child(f"{self.base}/{user_id}/{dest}").put(source)
+                bucket = storage.bucket()
+                blob = bucket.blob(f"{self.base}/{user_id}/{dest}")
+                blob.upload_from_filename(source)
+                # self.storage.child(f"{self.base}/{user_id}/{dest}").put(source)
+                # self.storage.child(f"{self.base}/{dest}").put(source)
                 reason = "Upload successful"
                 print(reason)
                 return True, reason
@@ -56,12 +62,14 @@ class FirebaseStorage(IStorage):
 
     def downloadFile(self, source: str, dest: str) -> Tuple[bool, str]:
         print(f"Downloading file ....{source}")
-        if self.auth.current_user is not None:
+        if self.current_user is not None:
             try:
-                user_id = self.auth.current_user["localId"]
+                user_id = self.current_user["localId"]
                 print(user_id)
                 path = f"{self.base}/{user_id}/{source}"
-                self.storage.child(path).download(dest)
+                bucket = storage.bucket()
+                blob = bucket.blob(path)
+                blob.download_to_filename(dest)
 
                 reason = "Download successful"
                 print(reason)
@@ -78,10 +86,14 @@ class FirebaseStorage(IStorage):
 
     def deleteFile(self, source: str) -> Tuple[bool, str]:
         print("Deleting file...")
-        if self.auth.current_user is not None:
+        if self.current_user is not None:
             try:
-                user_id = self.auth.current_user["localId"]
-                self.storage.delete(f"{self.base}/{user_id}/{source}")
+                user_id = self.current_user["localId"]
+                source = f"{self.base}/{user_id}/{source}"
+
+                bucket = storage.bucket()
+                blob = bucket.blob(source)
+                blob.delete()
                 reason = "File deleted successfully"
                 print(reason)
                 return True, reason
@@ -97,21 +109,20 @@ class FirebaseStorage(IStorage):
 
     def getFileURL(self, source: str) -> str:
         print("getting file URL and signing it...")
-        if self.auth.current_user is not None:
-            
-            user_id = self.auth.current_user["localId"]
-            bucket = self.storage.bucket
+        if self.current_user is not None:
+
+            user_id = self.current_user["localId"]
+            bucket = storage.bucket()
             blob = bucket.blob(f"{self.base}/{user_id}/{source}")
 
             file_exists = blob.exists()
             if file_exists:
-
                 encryption_key = datetime.now() + timedelta(days=10000)
                 signed_url = blob.generate_signed_url(encryption_key)
                 print(f"SignedURL is {signed_url}")
                 return signed_url
             else:
-                print(f"Path not found")
+                print("Path not found")
                 reason = "URL cannot be retrieved"
                 print(reason)
                 return reason
@@ -122,17 +133,17 @@ class FirebaseStorage(IStorage):
 
     def copyFile(self, source: str, dest: str) -> Tuple[bool, str]:
         print("Copying File....")
-        if self.auth.current_user is not None:
-            source_bucket = self.storage.bucket
-            user_id = self.auth.current_user["localId"]
+        if self.current_user is not None:
+            source_bucket = storage.bucket()
+            user_id = self.current_user["localId"]
             path = f"{self.base}/{user_id}/{source}"
             blob = source_bucket.blob(path)
             dest = f"{self.base}/{user_id}/{dest}"
-            dest_bucket = self.storage.bucket
+            dest_bucket = storage.bucket()
 
             try:
                 print(blob)
-                source_bucket.copy_blob(blob, dest_bucket, new_name=dest)
+                source_bucket.copy_blob(blob, dest_bucket, dest)
                 reason = "file copied successfully"
                 print(reason)
                 return True, reason
@@ -149,12 +160,12 @@ class FirebaseStorage(IStorage):
 
     def listFilesInDirectory(self, source: str) -> Tuple[bool, str, List[str]]:
         print("Listing files in directory...")
-        if self.auth.current_user is not None:
-            user_id = self.auth.current_user["localId"]
+        if self.current_user is not None:
+            user_id = self.current_user["localId"]
             dir_files = []
             path = f"{self.base}/{user_id}/{source}"
-            bucket = self.storage.bucket
-            
+            bucket = storage.bucket()
+
             list_files = bucket.list_blobs(prefix=path)
             new_list = list(list_files)
 
@@ -170,7 +181,7 @@ class FirebaseStorage(IStorage):
                 print(dir_files)
                 return True, reason, dir_files
             else:
-                print(f"Error: Path not found")
+                print("Error: Path not found")
                 reason = f"Cannot list files in {source}"
                 print(reason)
                 return False, reason, ""
@@ -181,9 +192,9 @@ class FirebaseStorage(IStorage):
 
     def checkIfFileExists(self, source: str) -> Tuple[bool, str]:
         print("Checking if file exists...")
-        if self.auth.current_user is not None:
-            bucket = self.storage.bucket
-            user_id = self.auth.current_user["localId"]
+        if self.current_user is not None:
+            bucket = storage.bucket()
+            user_id = self.current_user["localId"]
             source = f"{self.base}/{user_id}/{source}"
             blob = bucket.blob(source)
             file_exists = blob.exists()
@@ -203,9 +214,9 @@ class FirebaseStorage(IStorage):
 
     def createDirectory(self, source: str) -> Tuple[bool, str]:
         print("Creating Directory...")
-        if self.auth.current_user is not None:
-            user_id = self.auth.current_user["localId"]
-            bucket = self.storage.bucket
+        if self.current_user is not None:
+            user_id = self.current_user["localId"]
+            bucket = storage.bucket()
             new_path = f"{self.base}/{user_id}/{source}"
             list_files = bucket.list_blobs(prefix=new_path)
 
@@ -217,11 +228,15 @@ class FirebaseStorage(IStorage):
                 reason = "Failed to create directory"
                 print(reason)
                 return False, reason
-                
+
             else:
                 empty_dir = ".file"
                 path = f"{self.base}/{user_id}/{source}/{empty_dir}"
-                self.storage.child(path).put("data_download/.file")
+
+                # file_name is the location of file on local disk
+                file_name = "data_download/.file"
+                blob = bucket.blob(path)
+                blob.upload_from_filename(file_name)
                 reason = f"Directory {source} created"
                 print(reason)
                 return True, reason
@@ -233,9 +248,9 @@ class FirebaseStorage(IStorage):
     def deleteDirectory(self, source: str) -> Tuple[bool, str]:
         print("Deleting Directory...")
         print(f"Deleting source {source}")
-        if self.auth.current_user is not None:
-            user_id = self.auth.current_user["localId"]
-            bucket = self.storage.bucket
+        if self.current_user is not None:
+            user_id = self.current_user["localId"]
+            bucket = storage.bucket()
             path = f"{self.base}/{user_id}/{source}"
             check_list = []
 
@@ -269,10 +284,10 @@ class FirebaseStorage(IStorage):
 
     def renameFile(self, source: str, dest: str) -> Tuple[bool, str]:
         print("Renaming File...")
-        if self.auth.current_user is not None:
+        if self.current_user is not None:
             try:
-                user_id = self.auth.current_user["localId"]
-                bucket = self.storage.bucket
+                user_id = self.current_user["localId"]
+                bucket = storage.bucket()
                 source = f"{self.base}/{user_id}/{source}"
                 blob = bucket.blob(source)
 
@@ -298,7 +313,8 @@ class FirebaseStorage(IStorage):
         print(f"Creating user...{email}")
         try:
             # authenticate the user
-            self.auth.create_user_with_email_and_password(email, password)
+            # self.auth.create_user_with_email_and_password(email, password)
+            auth.create_user(email=email, password=password)
             reason = "User Created Successfully"
             print(reason)
             return True, reason
@@ -312,9 +328,17 @@ class FirebaseStorage(IStorage):
         """signin created user"""
         print(f"logging in user...{email}")
         try:
-            self.auth.sign_in_with_email_and_password(email, password)
+            # self.auth.sign_in_with_email_and_password(email, password)
+            request_ref = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={0}".format(self.api_key)
+            headers = {"content-type": "application/json; charset=UTF-8"}
+            data = json.dumps({"email": email, "password": password, "returnSecureToken": True})
+            request_object = requests.post(request_ref, headers=headers, data=data)
+            # raise_detailed_error(request_object)
+            self.current_user = request_object.json()
+            # return request_object.json()
+
             reason = "Sign In Was Successful"
-            print(self.auth.current_user)
+            print(self.current_user)
             print(reason)
             return True, reason
         except Exception as e:
